912bf0fbb815f89e3ed498d8f421858d7aee6412
sunjatin <asyaefurr@gmail.com>
2023-03-15 16:52:24 +0700

    Task 7

---
+++ pages/javascript/js/task7.js
@@ -0,0 +1,47 @@
+//Global variable coba
+//var nama = 'Adnan';
+/*let id = 1010;
+const alamat = 'mjk';
+
+alert(`Haloo ${nama}`);
+alert(`Id kamu ${id}`);
+alert(`Alamat kamu di ${alamat}`);*/
+
+//local variable coba
+/*function salam() {
+  let alamat = 'Mjk';
+  console.log('Hai ' + nama + 'dari ' + alamat);
+}*/
+
+// console.log(`Halo ${nama}`);
+
+//TIPE DATA
+//Number
+/*let bil1 = 20;
+let bil2 = 10;
+
+console.log(typeof bil1);
+console.log(typeof bil2);
+console.log(10 + 20);
+alert('Arkatama' + bil2);*/
+
+//STRING
+/*let nama = 'Adnan syaefur'; //btw yang ini tadinya ("..") tapi otomatis dirubah sama vscode jadi ('..')
+let stts = 'mencari jalan yang berujung';
+let hobi = `memanjat kenyataan`;
+
+console.log('Saya ' + nama + ' saat ini tengah ' + stts + ' punya hobi ' + hobi);*/
+
+//NULL
+/*let nama = null;
+console.log(nama);*/
+
+//OBJECT
+let employee = {
+  nama: 'Genta',
+  usia: 24,
+  pekerjaan: 'Web Dev',
+  sudahMenikah: true,
+  hobi: ['Hiking', 'Sleeping'],
+};
+console.log(`Namanya  ${employee.nama} dan dia merupakan ${employee.pekerjaan}`);
